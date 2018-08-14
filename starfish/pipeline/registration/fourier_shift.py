eb7cf6b7e0f60c77395d4cde575203244a14fd13
Brian <brian@Brians-MacBook-Air.local>
2018-08-14 00:25:17 -0700

    fix mess, whitespace errors

--- starfish/pipeline/registration/fourier_shift.py
+++ starfish/pipeline/registration/fourier_shift.py
@@ -88,17 +88,17 @@ def shift_im(im: np.ndarray, shift: np.ndarray) -> np.ndarray:
     im_shift = np.fft.ifftn(fim_shift)
     im_shift = im_shift.real
     # identify ranges that have been wrapped and set the values to zero
-    imshape= list(im_shift.shape)
+    imshape = list(im_shift.shape)
 
     full_index_list = [slice(si) for si in imshape]
+    shift = shift * -1
+    shifted_indices = [(np.arange(imshape[i]) - shift[i]) for i in range(len(imshape))]
 
-    shifted_indices=[(np.arange(imshape[i])-shift[i]) for i in range(len(imshape))]
-
-    index_masks=[np.logical_or(index_and_shape[0]<0,index_and_shape[0]>index_and_shape[1]) for index_and_shape in zip(shifted_indices,imshape)]
+    index_masks = [np.logical_or(index_and_shape[0] < 0, index_and_shape[0] > index_and_shape[1]) for index_and_shape in zip(shifted_indices, imshape)]
 
     for i, ignore in enumerate(imshape):
-        indexListToUse=list(fullIndexList)
-        indexListToUse[i] = indexList2[i]
-        im_shift[tuple(indexListToUse)]=0
+        index_list = list(full_index_list)
+        index_list[i] = index_masks[i]
+        im_shift[tuple(index_list)] = 0
 
     return im_shift
